a9a04e5b7df2da3631710bd506f1e152cacd018a
aibyomkar <omkarpotdar42@gmail.com>
2025-07-08 13:40:02 +0530

    model loaer

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -3,7 +3,8 @@ class ConfigLoader:
     def __init__(self):
         pass
     
-class ModelLoader:
+class ModelLoader(BaseModel):
 
-    def __init__(self):
-        pass
+    model_provider: Literal["groq", "openai"] = "groq"
+
+    config: Optional[ConfigLoader] = Field(default = None, exclude = True)

6536beba970abf86e6f79581ecb35bf285b6670e
aibyomkar <omkarpotdar42@gmail.com>
2025-07-16 21:37:09 +0530

    Enhance query_travel_agent: Save generated graph as PNG and improve response handling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from agent.agentic_workflow import GraphBuilder
 from fastapi.responses import JSONResponse
+import os
 
 app = FastAPI()
 
@@ -14,7 +15,25 @@ async def query_travel_agent(query: QueryRequest):
 
     try:
         print(query)
-
         graph = GraphBuilder(model_provider = 'groq')
-
         react_app = graph()
+
+        png_graph = react_app.get_graph().draw_mermaid_png()
+        with open('my_graph.png', 'wb') as f:
+            f.write(png_graph)
+        print(f'Graph saved as \'my_graph.png\' in {os.getcwd()}')
+
+        # Assuming request is a pydantic object like: {'question': 'your text'}
+        messages = {'messages': [query.question]}
+        output = react_app.invoke(messages)
+
+        # if result is dict with messages:
+        if isinstance(output, dict) and 'messages' in output:
+            final_output = output['messages'][-1].content #last ai response
+        else:
+            final_output = str(output)
+
+        return {'answer': final_output}
+    
+    except Exception as e:
+        return JSONResponse(status_code = 500, content = {'error': str(e)})
